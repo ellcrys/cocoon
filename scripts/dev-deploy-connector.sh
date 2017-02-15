b4c382eaaecfaa505301fc6630ab675292e4e52f
Kennedy Idialu <kennedyidialu@gmail.com>
2017-02-15 12:27:46 +0100

    install git, compile data files, glide update and build connector

--- scripts/dev-deploy-connector.sh
+++ scripts/dev-deploy-connector.sh
@@ -1,2 +1,11 @@
+git clone https://github.com/ncodes/cocoon
+git config --global http.https://gopkg.in.followRedirects true
+
+# compilte data directory to binary
+cd cocoon/core/data
+go get -u github.com/jteeuwen/go-bindata/...
+go-bindata --pkg data ./...
+
+cd ../connector
 glide update
 go build -o /bin/connector connector.go
