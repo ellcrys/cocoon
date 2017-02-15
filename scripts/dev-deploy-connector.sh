903efa49758ebc7209781ef4b651a4026791f931
Kennedy Idialu <kennedyidialu@gmail.com>
2017-02-15 14:15:01 +0100

    debug

--- scripts/dev-deploy-connector.sh
+++ scripts/dev-deploy-connector.sh
@@ -4,8 +4,8 @@ git config --global http.https://gopkg.in.followRedirects true
 # compilte data directory to binary
 cd cocoon/core/data
 go get -u github.com/jteeuwen/go-bindata/...
-go-bindata --pkg data ./...
+# go-bindata --pkg data ./...
 
-cd ../connector
-glide install
-go build -o /bin/connector connector.go
+# cd ../connector
+# glide install
+# go build -o /bin/connector connector.go
