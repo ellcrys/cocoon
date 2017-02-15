04622f796b0cd3cf1b0d95afe64cbfc791c9da80
Kennedy Idialu <kennedyidialu@gmail.com>
2017-02-15 17:59:45 +0100

    deploy connector

--- scripts/dev-deploy-connector-0.0.5.sh
+++ scripts/dev-deploy-connector-0.0.5.sh
@@ -1,3 +1,4 @@
+# pull cocoon source
 git clone https://github.com/ncodes/cocoon
 git config --global http.https://gopkg.in.followRedirects true
 
@@ -6,6 +7,10 @@ cd cocoon/core/data
 go get -u -v github.com/jteeuwen/go-bindata/...
 go-bindata --pkg data ./...
 
+# build connector
 cd ../connector
 glide install
 go build -o /bin/connector connector.go
+
+# start connector
+connector start
