c6c5efc39d6b0ce1ada930c4fa5a744c5f59afec
Kennedy Idialu <kennedyidialu@gmail.com>
2017-02-17 15:00:46 +0100

    include protocol

--- scripts/cocoon-group.sh
+++ scripts/cocoon-group.sh
@@ -1,18 +1,21 @@
-# Script to setup the cocoon docker image. (privlledged flag is required)
+# Script to setup the cocoon docker image. (privlledged flag is required).
+# This file must be executed during image building phase. 
 
 iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 
 # Default cocoon code group prevents access to connector server,
 # public internet
 groupdel cocoon && groupadd cocoon
-iptables -A OUTPUT -o lo --dport 3000 -m owner --gid-owner cocoon -j DROP
-iptables -A OUTPUT -o lo --dport 3001 -m owner --gid-owner cocoon -j ACCEPT
+iptables -A OUTPUT -o lo -p tcp --dport 3000 -m owner --gid-owner cocoon -j DROP
+iptables -A OUTPUT -o lo -p udp --dport 3000 -m owner --gid-owner cocoon -j DROP
+iptables -A OUTPUT -o lo -p tcp --dport 3001 -m owner --gid-owner cocoon -j ACCEPT
 iptables -A OUTPUT -m owner --gid-owner cocoon -j DROP
 
 # This cocoon group allows access to the public internet
 groupdel cocoon-open && groupdel cocoon-open
-iptables -A OUTPUT -o lo --dport 3000 -m owner --gid-owner cocoon-open -j DROP
-iptables -A OUTPUT -o lo --dport 3001 -m owner --gid-owner cocoon-open -j ACCEPT
+iptables -A OUTPUT -o lo -p tcp --dport 3000 -m owner --gid-owner cocoon-open -j DROP
+iptables -A OUTPUT -o lo -p udp --dport 3000 -m owner --gid-owner cocoon-open -j DROP
+iptables -A OUTPUT -o lo -p tcp --dport 3001 -m owner --gid-owner cocoon-open -j ACCEPT
 
 # save
 iptables-save > /etc/iptables/rules.v4
