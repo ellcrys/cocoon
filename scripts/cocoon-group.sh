31443fff4fd9088036b3f9a573f1680d9098c1e0
Kennedy Idialu <kennedyidialu@gmail.com>
2017-02-17 14:42:26 +0100

    delete group if exists

--- scripts/cocoon-group.sh
+++ scripts/cocoon-group.sh
@@ -4,12 +4,12 @@ iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 
 # Default cocoon code group prevents access to connector server,
 # public internet
-groupadd cocoon
+groupdel cocoon && groupadd cocoon
 iptables -A OUTPUT -o lo -dport 3000 -m owner --gid-owner cocoon -j DROP
 iptables -A OUTPUT -o lo -dport 3001 -m owner --gid-owner cocoon -j ACCEPT
 iptables -A OUTPUT -m owner --gid-owner cocoon -j DROP
 
 # This cocoon group allows access to the public internet
-groupadd cocoon-open
+groupdel cocoon-open && groupdel cocoon-open
 iptables -A OUTPUT -o lo -dport 3000 -m owner --gid-owner cocoon-open -j DROP
 iptables -A OUTPUT -o lo -dport 3001 -m owner --gid-owner cocoon-open -j ACCEPT
