147da915e3d6dbf0ab2c8b123a12370e6dcc36f3
Kennedy Idialu <kennedyidialu@gmail.com>
2017-02-18 21:30:23 +0100

    delete before adding

--- scripts/cocoon-fw.sh
+++ scripts/cocoon-fw.sh
@@ -1,5 +1,7 @@
 # Cocoon firewall directives
 
 # Drop all outgoing connections
+iptables -D DOCKER 1 -o docker0 -p tcp -j DROP
 iptables -I DOCKER 1 -o docker0 -p tcp -j DROP
+iptables -D DOCKER 1 -o docker0 -p udp -j DROP
 iptables -I DOCKER 1 -o docker0 -p udp -j DROP
