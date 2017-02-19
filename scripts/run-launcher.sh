9ff74641113fdcf6f4af84b8c406be6351fede96
Kennedy Idialu <kennedyidialu@gmail.com>
2017-02-19 16:31:17 +0100

    create new bridge for launcher

--- scripts/run-launcher.sh
+++ scripts/run-launcher.sh
@@ -1,5 +1,26 @@
-# start dockerd
-docker-entrypoint.sh dockerd &
+# setup docker.
+# Create custom bridge
+ip link set dev docker0 down
+brctl delbr docker0
+iptables -t nat -F POSTROUTING
+
+# Create bridge. Attempt to use the cocoon id as the bridge bridgeName
+# otherwise, continusly create new names till we find a unique one
+bridgeName=${COCOON_ID}
+for (( ; ; ))
+do  
+    brctl addbr bridgeName
+    if [$? -eq 0]
+        break
+    fi
+    randStr=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+    bridgeName="$(echo -n $randStr | sha1sum | awk '{print $1}' | cut -c1-15)"
+done
+
+ip addr add 192.168.5.1/24 dev $bridgeName
+sudo ip link set dev $bridgeName up
+
+docker-entrypoint.sh dockerd -b=$bridgeName &
 
 # give dockerd time to start
 sleep 10
