396970d2ceb8c4f6f914b5d76e957c7f99cb9549
Kennedy Idialu <kennedyidialu@gmail.com>
2017-02-19 19:41:22 +0100

    include necessary bridge forward rules

--- scripts/cocoon-fw.sh
+++ scripts/cocoon-fw.sh
@@ -8,3 +8,9 @@ if [ $? -eq 1 ]; then
     iptables -I DOCKER 1 -o $bridgeName -p tcp -j DROP
     iptables -I DOCKER 1 -o $bridgeName -p udp -j DROP
 fi
+
+# set forwarding rules  (TODO: find a way to delete this rules when the container shutsdown)
+iptables -A FORWARD -o $bridgeName -j DOCKER
+iptables -A FORWARD -o $bridgeName -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+iptables -A FORWARD -i $bridgeName ! -o $bridgeName -j ACCEPT
+iptables -A FORWARD -i $bridgeName -o $bridgeName -j ACCEPT
