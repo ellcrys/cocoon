f8fb4c876d37f15cce01858d16c041c6469213a3
Kennedy Idialu <kennedyidialu@gmail.com>
2017-02-19 14:42:53 +0100

    use conditions when setting firewall rules

--- scripts/cocoon-fw.sh
+++ scripts/cocoon-fw.sh
@@ -1,7 +1,11 @@
 # Cocoon firewall directives
 
 # Drop all outgoing connections
-iptables -D DOCKER -o docker0 -p tcp -j DROP
-iptables -I DOCKER 1 -o docker0 -p tcp -j DROP
-iptables -D DOCKER -o docker0 -p udp -j DROP
-iptables -I DOCKER 1 -o docker0 -p udp -j DROP
+iptables -C DOCKER -o docker0 -p tcp -j DROP
+if [ $? -eq 1 ]; then
+    iptables -I DOCKER 1 -o docker0 -p tcp -j DROP
+    iptables -I DOCKER 1 -o docker0 -p udp -j DROP
+else
+    iptables -D DOCKER -o docker0 -p tcp -j DROP
+    iptables -D DOCKER -o docker0 -p udp -j DROP
+fi
