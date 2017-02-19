eb52e64cc241f83409d5875b180214746e681b47
Kennedy Idialu <kennedyidialu@gmail.com>
2017-02-19 14:57:48 +0100

    only add rules if they don't exist

--- scripts/cocoon-fw.sh
+++ scripts/cocoon-fw.sh
@@ -5,7 +5,4 @@ iptables -C DOCKER -o docker0 -p tcp -j DROP
 if [ $? -eq 1 ]; then
     iptables -I DOCKER 1 -o docker0 -p tcp -j DROP
     iptables -I DOCKER 1 -o docker0 -p udp -j DROP
-else
-    iptables -D DOCKER -o docker0 -p tcp -j DROP
-    iptables -D DOCKER -o docker0 -p udp -j DROP
 fi
