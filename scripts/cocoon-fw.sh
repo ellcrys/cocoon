91bcb44c7ef54807a601c0274d3ecd216740c7de
Kennedy Idialu <kennedyidialu@gmail.com>
2017-02-20 20:05:39 +0100

    improved firewall script ensures udp and tcp connections are refused by default. It allows upd connections to nameserver.

--- scripts/cocoon-fw.sh
+++ scripts/cocoon-fw.sh
@@ -1,7 +1,6 @@
 # Cocoon firewall directives
 #TODO: find a way to delete this rules when the container shuts down
 
-# #################### DOCKER BRIDGE SETUP ################
 # Determine new bridge name. Use cocoon id as default bridge name
 # but continously find a bridge name if the default has been used.
 bridgeName="$(echo -n ${COCOON_ID} | awk '{print $1}' | cut -c1-15)"
@@ -15,29 +14,39 @@ do
     bridgeName="$(echo -n $randStr | sha1sum | awk '{print $1}' | cut -c1-15)"
 done
 
+export BRIDGE_NAME=$bridgeName
+
 # create new bridge
 ip addr add dev $bridgeName
 ip link set dev $bridgeName up
 echo "Create new bridge named: $bridgeName"
 
-################### END DOCKER BRIDE SETUP #######################
 
 # start docker daemon
 docker-entrypoint.sh dockerd -b=$bridgeName --iptables=false & 
 sleep 5
 echo "Started docker daemon"
 
-# Drop all outgoing connections
-iptables -C DOCKER -o $bridgeName -p tcp -j DROP
-if [ $? -eq 1 ]; then
-    iptables -I DOCKER 1 -o $bridgeName -p tcp -j DROP
-    iptables -I DOCKER 1 -o $bridgeName -p udp -j DROP
-fi
-
-# set iptable rules
+# forward bride packets to DOCKER chain
 iptables -A FORWARD -o $bridgeName -j DOCKER
 iptables -A FORWARD -o $bridgeName -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
-iptables -A FORWARD -i $bridgeName ! -o $bridgeName -j ACCEPT
+
+# Accept DNS lookup packets
+dnsIPs="$(cat /etc/resolv.conf | grep 'nameserver' | cut -c12-)"
+for ip in $dnsIPs
+do
+    iptables -A FORWARD -m state --state NEW,ESTABLISHED -d ${ip} -p udp --dport 53  -i $bridgeName -j ACCEPT
+    iptables -A FORWARD -m state --state ESTABLISHED -p udp -s ${ip} --sport 53 -o $bridgeName -j ACCEPT
+    iptables -A FORWARD -m state --state NEW,ESTABLISHED -d ${ip} -p tcp --dport 53  -i $bridgeName -j ACCEPT
+    iptables -A FORWARD -m state --state ESTABLISHED -p tcp -s ${ip} --sport 53  -o $bridgeName -j ACCEPT
+done
+
+# Block all udp and tcp packets
+iptables -A FORWARD -p udp  -i $bridgeName -j DROP
+iptables -A FORWARD -p tcp -i $bridgeName -j DROP
+
+# set container forwarding rules
+iptables -A FORWARD -i $bridgeName ! -o $bridgeName -j ACCEPT           # route tracfic from bride to any other available bridge (other containers & internet)
 iptables -A FORWARD -i $bridgeName -o $bridgeName -j ACCEPT
 
 # set postrouting rule
