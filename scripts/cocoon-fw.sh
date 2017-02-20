025d633fe66c3d37190fd25b61e393f3f3f3e311
Kennedy Idialu <kennedyidialu@gmail.com>
2017-02-20 08:27:26 +0100

    debug

--- scripts/cocoon-fw.sh
+++ scripts/cocoon-fw.sh
@@ -4,11 +4,11 @@
 bridgeName=${BRIDGE_NAME}
 
 # Drop all outgoing connections
-iptables -C DOCKER -o $bridgeName -p tcp -j DROP
-if [ $? -eq 1 ]; then
+# iptables -C DOCKER -o $bridgeName -p tcp -j DROP
+# if [ $? -eq 1 ]; then
     # iptables -I DOCKER 1 -o $bridgeName -p tcp -j DROP
     # iptables -I DOCKER 1 -o $bridgeName -p udp -j DROP
-fi
+# fi
 
 # set forwarding rules. 
 iptables -A FORWARD -o $bridgeName -j DOCKER
