e3862a496b6a3b12bfffceff3d25f473162c96d8
Kennedy Idialu <kennedyidialu@gmail.com>
2017-02-20 10:18:22 +0100

    Includes revamped iptable rules that support the inclusion of POSTROUTING rules for a cocoon.

--- scripts/cocoon-fw.sh
+++ scripts/cocoon-fw.sh
@@ -1,7 +1,27 @@
 # Cocoon firewall directives
 #TODO: find a way to delete this rules when the container shuts down
 
-bridgeName=${BRIDGE_NAME}
+# #################### DOCKER BRIDGE SETUP ################
+# Determine new bridge name. Use cocoon id as default bridge name
+# but continously find a bridge name if the default has been used.
+bridgeName="$(echo -n ${COCOON_ID} | awk '{print $1}' | cut -c1-15)"
+for (( ; ; ))
+do  
+    brctl addbr ${bridgeName}
+    if [ $? -eq 0 ]; then
+        break
+    fi
+    randStr=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+    bridgeName="$(echo -n $randStr | sha1sum | awk '{print $1}' | cut -c1-15)"
+done
+
+# create new bridge
+ip addr add dev $bridgeName
+ip link set dev $bridgeName up
+export BRIDGE_NAME=$bridgeName
+echo "Create new bridge named: $bridgeName"
+
+################### END DOCKER BRIDE SETUP #######################
 
 # Drop all outgoing connections
 # iptables -C DOCKER -o $bridgeName -p tcp -j DROP
@@ -10,8 +30,12 @@ bridgeName=${BRIDGE_NAME}
     # iptables -I DOCKER 1 -o $bridgeName -p udp -j DROP
 # fi
 
-# set forwarding rules. 
+# set iptable rules
 iptables -A FORWARD -o $bridgeName -j DOCKER
 iptables -A FORWARD -o $bridgeName -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
 iptables -A FORWARD -i $bridgeName ! -o $bridgeName -j ACCEPT
 iptables -A FORWARD -i $bridgeName -o $bridgeName -j ACCEPT
+
+# set postrouting rule
+iface="$(docker network inspect bridge | jq '.[0].IPAM.Config[0].Subnet')"
+iptables -t nat -A POSTROUTING -s $iface -j MASQUERADE
--- scripts/run-launcher.sh
+++ scripts/run-launcher.sh
@@ -1,41 +1,11 @@
 # Launch Script
 
-# #################### Docker Bridge Setup ################
-# ip addr show docker0
-# if [ $? -eq 0 ]; then
-    # ip link set dev docker0 down
-    #brctl delbr docker0
-    # iptables -t nat -F POSTROUTING
-# fi
+# Run cocoon firewall script
+bash ${NOMAD_META_SCRIPTS_DIR}/${NOMAD_META_COCOON_FIREWALL_SCRIPT_NAME}
 
-# Create bridge. Attempt to use the cocoon id as the bridge bridgeName
-# otherwise, continusly create new names till we find a unique one
-bridgeName=${COCOON_ID}
-for (( ; ; ))
-do  
-    brctl addbr ${bridgeName}
-    if [ $? -eq 0 ]; then
-        break
-    fi
-    randStr=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
-    bridgeName="$(echo -n $randStr | sha1sum | awk '{print $1}' | cut -c1-15)"
-done
-
-echo "Bridge Name: $bridgeName"
-
-export BRIDGE_NAME=$bridgeName
-
-ip addr add dev $bridgeName
-ip link set dev $bridgeName up
-
-docker-entrypoint.sh dockerd -b=$bridgeName  & 
-################### END DOCKER BRIDE SETUP #######################
-
-# give dockerd time to start
-sleep 10
-
-##################### SET COCOON FIREWALL AND GROUPS #############
-bash ${NOMAD_META_SCRIPTS_DIR}/${NOMAD_META_COCOON_GROUPS_SCRIPT_NAME}
+# start docker daemon
+docker-entrypoint.sh dockerd -b=${BRIDGE_NAME} --iptables=false & 
+echo "Started docker daemon"
 
 # pull cocoon source
 git clone -b master https://github.com/ncodes/cocoon
