fc0f1f9d389063f551dc472372e5dae3cb364ab9
Kennedy Idialu <kennedyidialu@gmail.com>
2017-02-23 10:16:47 +0100

    glide install

--- scripts/connector_run.sh
+++ scripts/connector_run.sh
@@ -5,4 +5,5 @@ export COCOON_CODE_URL=https://github.com/ncodes/cocoon-example-01
 export COCOON_CODE_LANG=go
 export COCOON_BUILD_PARAMS='{ "pkg_mgr": "glide" }'
 
+glide install
 go run connector.go start
