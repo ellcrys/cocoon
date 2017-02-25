7255a3c6b4284661380fa5b1df8706366e6160be
Kennedy Idialu <kennedyidialu@gmail.com>
2017-02-25 09:44:31 +0100

    remove trailing ampersand

--- scripts/run-connector.sh
+++ scripts/run-connector.sh
@@ -5,15 +5,15 @@ dockerd-entrypoint.sh dockerd --iptables=false || { echo 'failed to start docker
 echo "Started docker daemon"
 
 # pull cocoon source
-git clone -b master https://github.com/ncodes/cocoon || { echo 'failed to pull cocoon repository' ; exit 1; } & 
+git clone -b master https://github.com/ncodes/cocoon || { echo 'failed to pull cocoon repository' ; exit 1; }
 
 # pull launch-go image
-docker pull ncodes/launch-go:latest || { echo 'failed to pull ncodes/launch-go image' ; exit 1; } & 
+docker pull ncodes/launch-go:latest || { echo 'failed to pull ncodes/launch-go image' ; exit 1; }
 
 # build the connector
 cd cocoon/core/connector
-glide install || { echo 'glide install has failed' ; exit 1; } & 
-go build -o /bin/connector connector.go || { echo 'failed to build connector' ; exit 1; } & 
+glide install || { echo 'glide install has failed' ; exit 1; }
+go build -o /bin/connector connector.go || { echo 'failed to build connector' ; exit 1; }
 
 # start connector 
 connector start
