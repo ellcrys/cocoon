0da23434f80356dae3a823353a9dbe1e299068ed
Kennedy Idialu <kennedyidialu@gmail.com>
2017-02-25 09:05:46 +0100

    remove binary compiler

--- scripts/run-connector.sh
+++ scripts/run-connector.sh
@@ -7,16 +7,11 @@ echo "Started docker daemon"
 # pull cocoon source
 git clone -b master https://github.com/ncodes/cocoon
 
-# compile data directory to binary
-cd cocoon/core/data
-go get -u -v github.com/jteeuwen/go-bindata/...
-go-bindata --pkg data ./...
-
 # pull launch-go image
 docker pull ncodes/launch-go:latest
 
 # build the connector
-cd ../connector
+cd cocoon/core/connector
 glide install
 go build -o /bin/connector connector.go
 
