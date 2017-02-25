f824d8ebb136c70c44d2f2b995579dcee81e5ac8
Kennedy Idialu <kennedyidialu@gmail.com>
2017-02-25 12:22:38 +0100

    fix commands

--- scripts/run-connector.sh
+++ scripts/run-connector.sh
@@ -4,18 +4,18 @@
 bash dockerd-entrypoint.sh dockerd --iptables=false || { echo 'failed to start dockerd' ; exit 1; } & 
 echo "Started docker daemon"
 
-# pull cocoon source
-git clone -b master https://github.com/ncodes/cocoon || { echo 'failed to pull cocoon repository' ; exit 1; }
-
 # pull launch-go image
 docker pull ncodes/launch-go:latest || { echo 'failed to pull ncodes/launch-go image' ; exit 1; }
 
-# build the connector
-cd cocoon/core/connector
+# pull cocoon source
+git clone -b master https://github.com/ncodes/cocoon || { echo 'failed to pull cocoon repository' ; exit 1; }
+
+# build the binary
+cd cocoon
 glide install || { echo 'glide install has failed' ; exit 1; }
-go build -o /bin/connector connector.go || { echo 'failed to build connector' ; exit 1; }
+go build -o /bin/cocoon core/main.go || { echo 'failed to build cocoon binary' ; exit 1; }
 
 # start connector 
 repoHash=$(git rev-parse HEAD)
 echo "Cocoon Version: $repoHash"
-connector start
+cocoon connector
