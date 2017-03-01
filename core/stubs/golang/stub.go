b1a73175abb571561cf61d74e213d8f1f1d9cbb1
Kennedy Idialu <kennedyidialu@gmail.com>
2017-03-01 12:11:18 +0100

    Ongoing implementation for stub architecture to allow cocoon codes perform operations against the ledgerchain through the connector.

--- core/stubs/golang/stub.go
+++ core/stubs/golang/stub.go
@@ -6,9 +6,14 @@ import (
 
 	"os"
 
+	"time"
+
+	"github.com/ellcrys/util"
+	"github.com/ncodes/cocoon/core/ledgerchain/types"
 	"github.com/ncodes/cocoon/core/stubs/golang/config"
 	"github.com/ncodes/cocoon/core/stubs/golang/proto"
 	"github.com/op/go-logging"
+	cmap "github.com/orcaman/concurrent-map"
 	"google.golang.org/grpc"
 )
 
@@ -17,39 +22,27 @@ var defaultServer *stubServer
 var log *logging.Logger
 var serverDone chan bool
 
+// txChannels holds the channels to send transaction responses to
+var txRespChannels = cmap.New()
+
+// ErrOperationTimeout represents a timeout error that occurs when response
+// is not received from orderer in time.
+var ErrOperationTimeout = fmt.Errorf("operation timed out")
+
+// ErrNotConnected represents an error about the cocoon code not
+// having an active connection with the connector.
+var ErrNotConnected = fmt.Errorf("not connected to the connector")
+
+// TxListLedgers represents a message to list ledgers belonging to cocoon code
+const TxListLedgers = "LIST_LEDGERS"
+
+// TxCreateLedger represents a message to create a ledger
+const TxCreateLedger = "CREATE_LEDGER"
+
 func init() {
 	defaultServer = new(stubServer)
 	config.ConfigureLogger()
 	log = logging.MustGetLogger("ccode.stub")
-}
-
-// StartServer starts the stub server and
-// readys it for service processing.
-// Accepts a callback that is called when the server starts
-func StartServer(startedCb func()) {
-
-	serverDone = make(chan bool, 1)
-
-	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", serverPort))
-	if err != nil {
-		log.Fatalf("failed to listen on port=%d", serverPort)
-	}
-
-	log.Infof("Started stub service at port=%d", serverPort)
-	server := grpc.NewServer()
-	proto.RegisterStubServer(server, &stubServer{})
-	go server.Serve(lis)
-
-	startedCb()
-	<-serverDone
-	log.Info("Cocoon code stopped")
-	os.Exit(0)
-}
-
-// Stop stub and cocoon code
-func Stop() {
-	log.Info("Stopping cocoon code")
-	serverDone <- true
 }
 
 // StubServer defines the services of the stub's GRPC connection
@@ -75,4 +68,131 @@ func (s *stubServer) Transact(stream proto.Stub_TransactServer) error {
 			Name: "do something",
 		})
 	}
+}
+
+// StartServer starts the stub server and
+// readys it for service processing.
+// Accepts a callback that is called when the server starts
+func StartServer(startedCb func()) {
+
+	serverDone = make(chan bool, 1)
+
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", serverPort))
+	if err != nil {
+		log.Fatalf("failed to listen on port=%d", serverPort)
+	}
+
+	log.Infof("Started stub service at port=%d", serverPort)
+	server := grpc.NewServer()
+	proto.RegisterStubServer(server, &stubServer{})
+	go server.Serve(lis)
+
+	startedCb()
+	<-serverDone
+	log.Info("Cocoon code stopped")
+	os.Exit(0)
+}
+
+// GetLogger returns the stubs logger.
+func GetLogger() *logging.Logger {
+	return log
+}
+
+// sendTx sends a transaction to the orderer
+// and saves the response channel to for any response
+// when available.
+func sendTx(tx *proto.Tx, respCh chan *proto.Resp) error {
+	txRespChannels.Set(tx.GetId(), respCh)
+	if err := defaultServer.stream.Send(tx); err != nil {
+		txRespChannels.Remove(tx.GetId())
+		log.Debugf("Failed to send transaction [%s] to orderer. %s", tx.GetId(), err)
+		return err
+	}
+	log.Debugf("Successfully sent transaction [%s] to orderer", tx.GetId())
+	return nil
+}
+
+// Stop stub and cocoon code
+func Stop() {
+	log.Info("Stopping cocoon code")
+	defaultServer.stream = nil
+	serverDone <- true
+}
+
+// waitOnRespChan takes a response channel and waits for response
+// to be received from it. If no error occurs, it copies the response
+// to the response pointer passed to it or returns error if it waited
+// 3 minutes and still got no response.
+func waitOnRespChan(ch chan *proto.Resp, resp *proto.Resp) error {
+	for {
+		select {
+		case r := <-ch:
+			*resp = *r
+			return nil
+		case <-time.After(3 * time.Minute):
+			return ErrOperationTimeout
+		}
+	}
+}
+
+// isConnected checks if connection with the connector
+// is active.
+func isConnected() bool {
+	return defaultServer.stream != nil
+}
+
+// ListLedgers returns the list of ledgers created
+// by the current cocoon code.
+func ListLedgers() ([]*types.Ledger, error) {
+
+	if !isConnected() {
+		return nil, ErrNotConnected
+	}
+
+	var respCh = make(chan *proto.Resp)
+	err := sendTx(&proto.Tx{
+		Id:     util.UUID4(),
+		Name:   TxListLedgers,
+		Params: []string{},
+	}, respCh)
+	if err != nil {
+		return nil, fmt.Errorf("failed get ledger list. %s", err)
+	}
+
+	// wait for response
+	var resp *proto.Resp
+	err = waitOnRespChan(respCh, resp)
+
+	return nil, nil
+}
+
+// CreateLedger creates a new ledger
+func CreateLedger() (*types.Ledger, error) {
+
+	if !isConnected() {
+		return nil, ErrNotConnected
+	}
+
+	var respCh = make(chan *proto.Resp)
+
+	txID := util.UUID4()
+	err := sendTx(&proto.Tx{
+		Id:     txID,
+		Name:   TxCreateLedger,
+		Params: []string{},
+	}, respCh)
+	if err != nil {
+		return nil, fmt.Errorf("failed create ledger. %s", err)
+	}
+
+	log.Debug("Waiting for response for transaction %s", txID)
+	var resp *proto.Resp
+	err = waitOnRespChan(respCh, resp)
+	if err != nil {
+		log.Errorf("receiving message from transaction [%s] failed because: %s", txID, err)
+		return nil, err
+	}
+
+	log.Info("Got Response: %s", resp)
+	return nil, nil
 }
