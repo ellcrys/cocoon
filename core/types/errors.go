5447c4148d381fa36708150d824cb052f008fe67
Kennedy Idialu <kennedyidialu@gmail.com>
2017-03-14 08:49:51 +0100

    Adds new error checker method for detecting error due to concurrent access of a previous block and attempts to use a previous block hash twice

--- core/types/errors.go
+++ core/types/errors.go
@@ -1,6 +1,7 @@
 package types
 
 import "fmt"
+import "strings"
 
 // ErrIdentityNotFound indicates a non-existing identity
 var ErrIdentityNotFound = fmt.Errorf("identity not found")
@@ -19,3 +20,15 @@ var ErrChainNotFound = fmt.Errorf("chain not found")
 
 // ErrZeroTransactions indicates a transaction list has zeto transactions
 var ErrZeroTransactions = fmt.Errorf("zero transactions not allowed")
+
+// IsPrevBlockConcurrentAccessError checks whether an error is one created when
+// attempt is made to access a previous block that is currently being updated by a concurrent transaction.
+func IsPrevBlockConcurrentAccessError(err error) bool {
+	return err.Error() == "failed to update previous block right sibling column. pq: could not serialize access due to concurrent update"
+}
+
+// IsDuplicatePrevHashError checks whether an error is one created when a transaction attempts to
+// create a record with a prev hash value already used by a previous commited transaction.
+func IsDuplicatePrevHashError(err error) bool {
+	return strings.Contains(err.Error(), `pq: duplicate key value violates unique constraint "idx_name_prev_hash"`)
+}
