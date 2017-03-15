cd498cb2870ce3bdabb72ceb0c9f4b306ebc4dcf
Kennedy Idialu <kennedyidialu@gmail.com>
2017-03-15 18:32:27 +0100

    Now recovers from panic() originating from the cocoon code Invoke(). As a result, a cocoon code will not exit due to panics within the Invoke() cocoon code method.

--- core/stubs/golang/server.go
+++ core/stubs/golang/server.go
@@ -67,24 +67,48 @@ func (s *stubServer) handleInvokeTransaction(tx *proto.Tx) error {
 			return fmt.Errorf("cocoon code is not running. Did you call the Run() method?")
 		}
 
-		functionName := tx.GetParams()[0]
-		result, err := ccode.Invoke(tx.GetId(), functionName, tx.GetParams()[1:])
-		if err != nil {
-			return err
-		}
+		var err error
+		var resp *proto.Tx
 
-		// coerce result to json
-		resultJSON, err := util.ToJSON(result)
-		if err != nil {
-			return fmt.Errorf("failed to coerce cocoon code Invoke() result to json string. %s", err)
-		}
+		// This closure allows us to catch panic from the cocoon code Invoke() method
+		// so cocoon codes will always continue to run
+		func() {
 
-		return s.stream.Send(&proto.Tx{
-			Id:       tx.GetId(),
-			Response: true,
-			Status:   200,
-			Body:     resultJSON,
-		})
+			defer func() {
+				if r := recover(); r != nil {
+					err = r.(error)
+					log.Errorf("Invoke() panicked: %s", err)
+					resp = &proto.Tx{
+						Id:       tx.GetId(),
+						Response: true,
+						Status:   500,
+						Body:     []byte("failed to complete invoke request"),
+					}
+				}
+			}()
+
+			functionName := tx.GetParams()[0]
+			result, err := ccode.Invoke(tx.GetId(), functionName, tx.GetParams()[1:])
+			if err != nil {
+				return
+			}
+
+			// coerce result to json
+			resultJSON, err := util.ToJSON(result)
+			if err != nil {
+				err = fmt.Errorf("failed to coerce cocoon code Invoke() result to json string. %s", err)
+				return
+			}
+
+			resp = &proto.Tx{
+				Id:       tx.GetId(),
+				Response: true,
+				Status:   200,
+				Body:     resultJSON,
+			}
+		}()
+
+		return s.stream.Send(resp)
 
 	default:
 		return fmt.Errorf("Unsupported invoke transaction (%s)", tx.GetName())
