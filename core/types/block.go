7aa02961e952ee7975b6d85a0e33eb4760cc7389
Kennedy Idialu <kennedyidialu@gmail.com>
2017-03-18 08:54:39 +0100

    Added `GetTransactions` to convert transactions from bytes to slice of transactions

--- core/types/block.go
+++ core/types/block.go
@@ -1,5 +1,8 @@
 package types
 
+import "github.com/ellcrys/util"
+import "fmt"
+
 // Block represents a table of chains
 type Block struct {
 	PK            uint   `json:"-" gorm:"primary_key"`
@@ -8,6 +11,15 @@ type Block struct {
 	ChainName     string `json:"chainName" gorm:"index:idx_name_chain_name"`
 	PrevBlockHash string `json:"prevBlockHash" gorm:"type:varchar(64);unique_index:idx_name_prev_block_hash"`
 	Hash          string `json:"hash" gorm:"type:varchar(64);unique_index:idx_name_hash"`
-	Transactions  []byte `json:"txs,omitempty"`
+	Transactions  []byte `json:"transactions,omitempty"`
 	CreatedAt     int64  `json:"createdAt" gorm:"index:idx_name_created_at"`
+}
+
+// GetTransactions returns a slice of transactions in the block.
+func (b *Block) GetTransactions() ([]*Transaction, error) {
+	var txs []*Transaction
+	if err := util.FromJSON(b.Transactions, &txs); err != nil {
+		return nil, fmt.Errorf("failed to parse transactions")
+	}
+	return txs, nil
 }
