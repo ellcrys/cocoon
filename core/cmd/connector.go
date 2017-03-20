de05f7a41eaf30a879a7c74c44d69be93cef49da
Kennedy Idialu <kennedyidialu@gmail.com>
2017-03-20 17:58:02 +0100

    Update port key and usage

--- core/cmd/connector.go
+++ core/cmd/connector.go
@@ -73,13 +73,13 @@ var connectorCmd = &cobra.Command{
 
 		// start grpc API server
 		grpcServer := server.NewAPIServer(lchr)
-		grpcServerAddr := util.Env("NOMAD_IP_connector", "")
-		grpcServerPort := util.Env("NOMAD_PORT_connector", "8002")
+		grpcServerAddr := util.Env("NOMAD_IP_connector_grpc", "")
+		grpcServerPort := util.Env("NOMAD_PORT_connector_grpc", "8002")
 		go grpcServer.Start(fmt.Sprintf("%s:%s", grpcServerAddr, grpcServerPort), make(chan bool, 1))
 
 		httpServer := server.NewHTTPServer()
-		httpServerAddr := util.Env("NOMAD_IP_connector-http", "")
-		httpServerPort := util.Env("NOMAD_PORT_connector-http", "8003")
+		httpServerAddr := util.Env("NOMAD_IP_connector_http", "")
+		httpServerPort := util.Env("NOMAD_PORT_connector_http", "8003")
 		go httpServer.Start(fmt.Sprintf("%s:%s", httpServerAddr, httpServerPort))
 
 		if <-waitCh {
