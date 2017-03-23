e0ab74def74b4f8bc202fea9a97176cbc46136d4
Kennedy Idialu <kennedyidialu@gmail.com>
2017-03-23 07:55:28 +0100

    Run the orderer

--- scripts/config/orderer/run.sh
+++ scripts/config/orderer/run.sh
@@ -1,5 +1,6 @@
 # Run an orderer. 
-# Expects the following variables to be use to configure the orderer. 
+# Expects the following variables to be use to configure the orderer. This should
+# typically be set by a scheduler.
 # ORDERER_ADDR=127.0.0.1:8001
 # STORE_CON_STR=host=localhost user=ned dbname=cocoon sslmode=disable password=
 
@@ -13,4 +14,4 @@ git clone --depth=1 https://github.com/ncodes/cocoon
 # start the orderer
 cd cocoon
 glide install
-go run core/main.go
+go run core/main.go orderer
