ce67f45ddff9c6a954ffc5505643a8e69957397e
Kennedy Idialu <kennedyidialu@gmail.com>
2017-03-23 08:33:53 +0100

    Use IP and Port provided by scheduler

--- core/cmd/orderer.go
+++ core/cmd/orderer.go
@@ -1,6 +1,8 @@
 package cmd
 
 import (
+	"fmt"
+
 	"github.com/ellcrys/util"
 	b_impl "github.com/ncodes/cocoon/core/blockchain/impl"
 	"github.com/ncodes/cocoon/core/orderer"
@@ -18,14 +20,15 @@ var ordererCmd = &cobra.Command{
 
 		var log = logging.MustGetLogger("orderer")
 		log.Info("Orderer has started")
-		addr := util.Env("ORDERER_ADDR", "127.0.0.1:8001")
+		ip := util.Env("NOMAD_IP_orderer_grpc", "127.0.0.1")
+		port := util.Env("NOMAD_PORT_orderer_grpc", "8001")
 		storeConStr := util.Env("STORE_CON_STR", "host=localhost user=ned dbname=cocoon sslmode=disable password=")
 
 		endedCh := make(chan bool)
 		newOrderer := orderer.NewOrderer()
 		newOrderer.SetStore(new(impl.PostgresStore))
 		newOrderer.SetBlockchain(new(b_impl.PostgresBlockchain))
-		go newOrderer.Start(addr, storeConStr, endedCh)
+		go newOrderer.Start(fmt.Sprintf("%s:%s", ip, port), storeConStr, endedCh)
 
 		<-endedCh
 	},
