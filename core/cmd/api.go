7498ac6f3e23c90a6d55bfa3c86e73b3b0bfcf64
Kennedy Idialu <kennedyidialu@gmail.com>
2017-03-24 13:08:20 +0100

    debug

--- core/cmd/api.go
+++ core/cmd/api.go
@@ -64,7 +64,7 @@ var apiCmdStart = &cobra.Command{
 
 		// set bind address from environment var set by scheduler
 		if len(bindAddr) == 0 {
-			ip := util.Env(scheduler.Getenv("IP_API_RPC"), "127.0.0.1")
+			ip := util.Env(scheduler.Getenv("IP_API_RPC_"), "127.0.0.1")
 			port := util.Env(scheduler.Getenv("PORT_API_RPC"), "8005")
 			bindAddr = fmt.Sprintf("%s:%s", ip, port)
 		}
