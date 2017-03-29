82f4c93248b78ebd4bd49b8f123a4713aed9072c
Kennedy Idialu <kennedyidialu@gmail.com>
2017-03-29 13:36:20 +0100

    Start nomad and consul

--- scripts/dev-setup.sh
+++ scripts/dev-setup.sh
@@ -1,5 +1,7 @@
 # Bootstrap the entire platform for development or test
 
+consul agent -dev & sudo nomad agent -config=config/nomad/server.hcl & sudo nomad agent -config=config/nomad/client.hcl
+
 # Run the orderer
 nomad run config/orderer/orderer.nomad
 
