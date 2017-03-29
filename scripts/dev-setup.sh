5af48c7c242561791d02e629f5bbd265173798ac
Kennedy Idialu <kennedyidialu@gmail.com>
2017-03-29 16:57:56 +0100

    removed consul and nomad exec command

--- scripts/dev-setup.sh
+++ scripts/dev-setup.sh
@@ -1,7 +1,5 @@
 # Bootstrap the entire platform for development or test
 
-consul agent -dev & sudo nomad agent -config=config/nomad/server.hcl & sudo nomad agent -config=config/nomad/client.hcl &
-
 # Run the orderer
 nomad run config/orderer/orderer.nomad
 
