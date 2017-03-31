5ca7afd8cd01a1da8938f0d4f89806484632e2e4
Kennedy Idialu <kennedyidialu@gmail.com>
2017-03-31 20:40:32 +0100

    debug: peep dir

--- scripts/run-connector.sh
+++ scripts/run-connector.sh
@@ -8,23 +8,23 @@ export GOROOT=/go
 export GOPATH=/gocode
 PATH=$PATH:$GOROOT/bin:$GOPATH/bin
 mkdir -p $GOPATH/bin
-
+ls /
 # Pull cocoon source
-branch="master"
-repoOwner=github.com/ncodes
-repoOwnerDir=$GOPATH/src/$repoOwner
-mkdir -p $repoOwnerDir
-cd $repoOwnerDir
-printf "> Fetching cocoon source. [branch=$branch] [dest=$repoOwnerDir]\n"
-git clone --depth=1 -b $branch https://$repoOwner/cocoon
+# branch="master"
+# repoOwner=github.com/ncodes
+# repoOwnerDir=$GOPATH/src/$repoOwner
+# mkdir -p $repoOwnerDir
+# cd $repoOwnerDir
+# printf "> Fetching cocoon source. [branch=$branch] [dest=$repoOwnerDir]\n"
+# git clone --depth=1 -b $branch https://$repoOwner/cocoon
 
-# build the binary
-printf "> Building cocoon"
-cd cocoon
-rm -rf .glide/ && rm -rf vendor
-glide --debug install
-/go/bin/go build -v -o $GOPATH/bin/cocoon core/main.go
+# # build the binary
+# printf "> Building cocoon"
+# cd cocoon
+# rm -rf .glide/ && rm -rf vendor
+# glide --debug install
+# /go/bin/go build -v -o $GOPATH/bin/cocoon core/main.go
 
-# start connector 
-printf "Running Cocoon Connector"
-cocoon connector
+# # start connector 
+# printf "Running Cocoon Connector"
+# cocoon connector
