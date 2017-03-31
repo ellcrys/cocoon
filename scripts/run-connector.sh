9181d52da8721009d1d505e96157347997bfbcd2
Kennedy Idialu <kennedyidialu@gmail.com>
2017-03-31 22:17:48 +0100

    removed command to start dockerd. Added command to pull image

--- scripts/run-connector.sh
+++ scripts/run-connector.sh
@@ -16,6 +16,7 @@ repoOwnerDir=$GOPATH/src/$repoOwner
 mkdir -p $repoOwnerDir
 cd $repoOwnerDir
 printf "> Fetching cocoon source. [branch=$branch] [dest=$repoOwnerDir]\n"
+rm -rf cocoon
 git clone --depth=1 -b $branch https://$repoOwner/cocoon
 
 # build the binary
@@ -25,10 +26,8 @@ rm -rf .glide/ && rm -rf vendor
 glide --debug install
 /go/bin/go build -v -o $GOPATH/bin/cocoon core/main.go
 
-# start docker daemon
-printf "> Starting docker daemon \n"
-bash dockerd &
-sleep 5
+# pull launch-go image
+docker pull ncodes/launch-go:latest
 
 # start connector 
 printf "Running Cocoon Connector"
