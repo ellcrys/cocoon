fd70e09b130f2ba8de1e715c66446725613b8773
Kennedy Idialu <kennedyidialu@gmail.com>
2017-03-31 17:56:57 +0100

    debug

--- scripts/run-connector.sh
+++ scripts/run-connector.sh
@@ -2,22 +2,24 @@
 # Run the connector 
 set -e
 
+touch /go/hello.txt
+ls /go/hello.txt
 # Pull cocoon source
-branch="master"
-repoOwner=github.com/ncodes
-repoOwnerDir=/go/src/$repoOwner
-sudo mkdir -p $repoOwnerDir
-cd $repoOwnerDir
-printf "> Fetching cocoon source. [branch=$branch] [dest=$repoOwnerDir]\n"
-git clone --depth=1 -b $branch https://$repoOwner/cocoon
+# branch="master"
+# repoOwner=github.com/ncodes
+# repoOwnerDir=/go/src/$repoOwner
+# sudo mkdir -p $repoOwnerDir
+# cd $repoOwnerDir
+# printf "> Fetching cocoon source. [branch=$branch] [dest=$repoOwnerDir]\n"
+# git clone --depth=1 -b $branch https://$repoOwner/cocoon
 
-# build the binary
-printf "> Building cocoon"
-cd cocoon
-rm -rf .glide/ && rm -rf vendor
-glide --debug install
-go build -v -o /bin/cocoon core/main.go
+# # build the binary
+# printf "> Building cocoon"
+# cd cocoon
+# rm -rf .glide/ && rm -rf vendor
+# glide --debug install
+# go build -v -o /bin/cocoon core/main.go
 
-# start connector 
-printf "Running Cocoon Connector"
-cocoon connector
+# # start connector 
+# printf "Running Cocoon Connector"
+# cocoon connector
