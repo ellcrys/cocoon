05fdb30d8f7e35e7f3c2c53f4cc6c4686d388da4
Kennedy Idialu <kennedyidialu@gmail.com>
2017-03-31 23:37:21 +0100

    debug: use `exec`

--- scripts/run-connector.sh
+++ scripts/run-connector.sh
@@ -3,44 +3,32 @@
 set -e
 
 
-# # Set up go environment
-# export GOROOT=/go
-# export GOPATH=/gocode
-# PATH=$PATH:$GOROOT/bin:$GOPATH/bin
-# mkdir -p $GOPATH/bin
+# Set up go environment
+export GOROOT=/go
+export GOPATH=/gocode
+PATH=$PATH:$GOROOT/bin:$GOPATH/bin
+mkdir -p $GOPATH/bin
 
-# # Pull cocoon source
-# branch="master"
-# repoOwner=github.com/ncodes
-# repoOwnerDir=$GOPATH/src/$repoOwner
-# mkdir -p $repoOwnerDir
-# cd $repoOwnerDir
-# printf "> Fetching cocoon source. [branch=$branch] [dest=$repoOwnerDir]\n"
-# rm -rf cocoon
-# git clone --depth=1 -b $branch https://$repoOwner/cocoon
+# Pull cocoon source
+branch="master"
+repoOwner=github.com/ncodes
+repoOwnerDir=$GOPATH/src/$repoOwner
+mkdir -p $repoOwnerDir
+cd $repoOwnerDir
+printf "> Fetching cocoon source. [branch=$branch] [dest=$repoOwnerDir]\n"
+rm -rf cocoon
+git clone --depth=1 -b $branch https://$repoOwner/cocoon
 
-# # build the binary
-# printf "> Building cocoon"
-# cd cocoon
-# rm -rf .glide/ && rm -rf vendor
-# glide --debug install
-# /go/bin/go build -v -o $GOPATH/bin/cocoon core/main.go
+# build the binary
+printf "> Building cocoon"
+cd cocoon
+rm -rf .glide/ && rm -rf vendor
+glide --debug install
+/go/bin/go build -v -o $GOPATH/bin/cocoon core/main.go
 
-# # pull launch-go image
-# docker pull ncodes/launch-go:latest 
+# pull launch-go image
+docker pull ncodes/launch-go:latest 
 
-terminate() {
-    printf "Terminate received!!!!\n"
-    # kill -TERM "$CPID" 2>/dev/null
-    exit 0;
-}
-
-trap terminate TERM
-sleep 1h
-
-# # start connector 
-# printf "Running Cocoon Connector"
-# cocoon connector &
-# CPID=$!
-# printf "Waiting!"
-# wait "$CPID"
+# start connector 
+printf "Running Cocoon Connector"
+exec cocoon connector 
