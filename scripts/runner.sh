14d8ae13fd85816520d8e1f5e24202d048b755a7
Kennedy Idialu <kennedyidialu@gmail.com>
2017-04-06 14:50:20 +0100

    debug

--- scripts/runner.sh
+++ scripts/runner.sh
@@ -8,6 +8,5 @@ trap 'echo Receive itttt' SIGTERM SIGINT
 # Fetch the script and run it.
 rm -f $RUN_SCRIPT_NAME
 wget $RUN_SCRIPT_URL
-while true; do :; done
 chmod +x $RUN_SCRIPT_NAME
 ./$RUN_SCRIPT_NAME
