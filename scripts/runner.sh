bf997f0b752fba8f7cc4767a133eff7bb6be11d8
Kennedy Idialu <kennedyidialu@gmail.com>
2017-04-06 14:59:03 +0100

    debug

--- scripts/runner.sh
+++ scripts/runner.sh
@@ -9,4 +9,8 @@ trap 'echo Receive itttt' SIGTERM SIGINT
 rm -f $RUN_SCRIPT_NAME
 wget $RUN_SCRIPT_URL
 chmod +x $RUN_SCRIPT_NAME
-./$RUN_SCRIPT_NAME
+./$RUN_SCRIPT_NAME &
+PID=$!
+wait $PID
+wait $PID
+EXIT_STATUS=$?
