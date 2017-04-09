053684b1a33891daa24ba8e436fdd07ffd04b70a
Kennedy Idialu <kennedyidialu@gmail.com>
2017-04-09 12:56:28 +0100

    Added new ACL rule interpreter with tests

---
+++ core/connector/server/acl/interpreter.go
@@ -0,0 +1,198 @@
+package acl
+
+import (
+	"fmt"
+
+	"strings"
+
+	"github.com/ellcrys/util"
+	"github.com/ncodes/cocoon/core/types"
+)
+
+var (
+	// PrivAllow allows all operations
+	PrivAllow = "allow"
+	// PrivAllowCreateLedger allows a ledger creation operation
+	PrivAllowCreateLedger = "allow-create-ledger"
+	// PrivAllowPut allows a put operation
+	PrivAllowPut = "allow-put"
+	// PrivAllowGet allows a get operation
+	PrivAllowGet = "allow-get"
+	// PrivDeny denies all operations
+	PrivDeny = "deny"
+	// PrivDenyCreateLedger denies a ledger creation operation
+	PrivDenyCreateLedger = "deny-create-ledger"
+	// PrivDenyPut denies a put operation
+	PrivDenyPut = "deny-put"
+	// PrivDenyGet denies a get operation
+	PrivDenyGet = "deny-get"
+
+	// validPrivileges refers to the accepted/known privileges
+	validPrivileges = []string{
+		PrivAllow,
+		PrivAllowCreateLedger,
+		PrivAllowPut,
+		PrivAllowGet,
+		PrivDeny,
+		PrivDenyCreateLedger,
+		PrivDenyPut,
+		PrivDenyGet,
+	}
+)
+
+// Interpreter represents an ACL rule interpreter.
+type Interpreter struct {
+	rules         map[string]interface{}
+	defaultPolicy bool
+}
+
+// NewInterpreter creates a new ACLInterpreter object
+func NewInterpreter(rules map[string]interface{}, defaultPolicy bool) *Interpreter {
+	return &Interpreter{rules: rules, defaultPolicy: defaultPolicy}
+}
+
+// isValidLedgerKeyType checks whether the value type of a ledger rule is
+// the expected type of string or map of string
+func (i *Interpreter) isValidLedgerKeyType(v interface{}) bool {
+	switch v.(type) {
+	case string, map[string]string:
+		return true
+	default:
+		return false
+	}
+}
+
+// isValidPrivilege checks whether a privilege is valid
+func (i *Interpreter) isValidPrivilege(p string) bool {
+	return util.InStringSlice(validPrivileges, strings.TrimSpace(strings.ToLower(p)))
+}
+
+// isValidActorID checks whether a user/cocoon id is valid
+func (i *Interpreter) isValidActorID(u string) error {
+	if len(u) == 0 {
+		return fmt.Errorf("cocoon or identity id cannot be an empty string")
+	}
+	return nil
+}
+
+// Validate takes acl rules and checks whether it is value
+func (i *Interpreter) Validate() []error {
+	var errs []error
+	for ledgerName, val := range i.rules {
+
+		if ledgerName == "*" {
+			if _, ok := val.(string); !ok {
+				errs = append(errs, fmt.Errorf("%s: invalid wildcard ledger value type. Expects string value", ledgerName))
+			}
+		}
+
+		if !i.isValidLedgerKeyType(val) {
+			errs = append(errs, fmt.Errorf("%s: invalid ledger value type. Expects string or map of strings", ledgerName))
+		}
+
+		if ruleVal, ok := val.(string); ok {
+			privileges := strings.Split(ruleVal, " ")
+			for _, priv := range privileges {
+				if !i.isValidPrivilege(priv) {
+					errs = append(errs, fmt.Errorf("%s: ledger contains an invalid privilege (%s)", ledgerName, priv))
+				}
+			}
+		}
+
+		if actorsPrivileges, ok := val.(map[string]string); ok {
+			for user, _privileges := range actorsPrivileges {
+				if err := i.isValidActorID(user); err != nil {
+					errs = append(errs, fmt.Errorf("%s: invalid actor id. %s", ledgerName, err))
+				}
+				privileges := strings.Split(_privileges, " ")
+				for _, priv := range privileges {
+					if !i.isValidPrivilege(priv) {
+						errs = append(errs, fmt.Errorf("%s: ledger actor contains an invalid privilege (%s)", ledgerName, priv))
+					}
+				}
+			}
+		}
+	}
+	return errs
+}
+
+// isPermitted takes a privilege and an operation and returns
+// 0 if the operation is not allowed, 1 if the operation is allowed
+// and -1 if the privilege and operation combo has no permission rule logic.
+func (i *Interpreter) isPermitted(privilege, operation string) int {
+	if privilege == PrivAllow {
+		return 1
+	} else if privilege == PrivDeny {
+		return 0
+	}
+	if privilege == PrivAllowCreateLedger && operation == types.TxCreateLedger {
+		return 1
+	} else if privilege == PrivDenyCreateLedger && operation == types.TxCreateLedger {
+		return 0
+	}
+	if privilege == PrivAllowGet && util.InStringSlice([]string{types.TxGet, types.TxGetByID, types.TxGetLedger, types.TxGetBlockByID, types.TxRangeGet}, operation) {
+		return 1
+	} else if privilege == PrivDenyGet && util.InStringSlice([]string{types.TxGet, types.TxGetByID, types.TxGetLedger, types.TxGetBlockByID, types.TxRangeGet}, operation) {
+		return 0
+	}
+	if privilege == PrivAllowPut && operation == types.TxPut {
+		return 1
+	} else if privilege == PrivDenyPut && operation == types.TxPut {
+		return 0
+	}
+	return -1
+}
+
+// IsAllowed checks whether an operation is permitted. The supported
+// operations are all Tx operations in the types/transactions.go file.
+// If actorID is set, the specific actor rule takes precedence over the
+// wildcard rule (if set). If no rule is found for an operation and no
+// wildcard ledger rule, the operation is considered allowed.
+func (i *Interpreter) IsAllowed(ledgerName, actorID, operation string) bool {
+
+	var allowed = i.defaultPolicy
+
+	// check if wildcard ledger is set, if yes, check if it permits the operation
+	if allLedgersPrivileges, ok := i.rules["*"]; ok {
+		privileges := strings.Split(allLedgersPrivileges.(string), " ")
+		for _, p := range privileges {
+			switch i.isPermitted(strings.TrimSpace(p), operation) {
+			case 0:
+				allowed = false
+			case 1:
+				allowed = true
+			}
+		}
+	}
+
+	// check with ledger-specific privilege if provided
+	if ledgerPrivileges, ok := i.rules[ledgerName]; ok {
+		if allPrivileges, ok := ledgerPrivileges.(string); ok {
+			privileges := strings.Split(allPrivileges, " ")
+			for _, p := range privileges {
+				switch i.isPermitted(strings.TrimSpace(p), operation) {
+				case 0:
+					allowed = false
+				case 1:
+					allowed = true
+				}
+			}
+		}
+
+		if actorsPrivileges, ok := ledgerPrivileges.(map[string]string); ok {
+			if actorPrivileges, ok := actorsPrivileges[actorID]; ok {
+				privileges := strings.Split(actorPrivileges, " ")
+				for _, p := range privileges {
+					switch i.isPermitted(strings.TrimSpace(p), operation) {
+					case 0:
+						allowed = false
+					case 1:
+						allowed = true
+					}
+				}
+			}
+		}
+	}
+
+	return allowed
+}
---
+++ core/connector/server/acl/interpreter_test.go
@@ -0,0 +1,195 @@
+package acl
+
+import (
+	"testing"
+
+	"github.com/ncodes/cocoon/core/types"
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestInterpreter(t *testing.T) {
+	Convey("Interpreter", t, func() {
+
+		Convey(".Validate", func() {
+
+			Convey("Should return nil if rule is empty", func() {
+				i := NewInterpreter(map[string]interface{}{}, false)
+				So(i.Validate(), ShouldBeNil)
+			})
+
+			Convey("Should return err if ledger rule value has unexpected type", func() {
+				i := NewInterpreter(map[string]interface{}{"ledger1": 1}, false)
+				errs := i.Validate()
+				So(errs, ShouldNotBeEmpty)
+				So(errs[0].Error(), ShouldEqual, "ledger1: invalid ledger value type. Expects string or map of strings")
+				i = NewInterpreter(map[string]interface{}{"ledger1": map[string]interface{}{}}, false)
+				errs = i.Validate()
+				So(errs, ShouldNotBeEmpty)
+				So(errs[0].Error(), ShouldEqual, "ledger1: invalid ledger value type. Expects string or map of strings")
+			})
+
+			Convey("Should return err if wildcard ledger rule value type is not string", func() {
+				i := NewInterpreter(map[string]interface{}{"*": 1}, false)
+				errs := i.Validate()
+				So(errs, ShouldNotBeEmpty)
+				So(errs[0].Error(), ShouldEqual, "*: invalid wildcard ledger value type. Expects string value")
+			})
+
+			Convey("Should return err if ledger rule contains invalid privileges", func() {
+				i := NewInterpreter(map[string]interface{}{"ledger1": "something unknown"}, false)
+				errs := i.Validate()
+				So(len(errs), ShouldEqual, 2)
+				So(errs[0].Error(), ShouldEqual, "ledger1: ledger contains an invalid privilege (something)")
+				So(errs[1].Error(), ShouldEqual, "ledger1: ledger contains an invalid privilege (unknown)")
+			})
+
+			Convey("Should return error if legder rule contains invalid actor id", func() {
+				i := NewInterpreter(map[string]interface{}{
+					"ledger1": map[string]string{
+						"": "allow",
+					},
+				}, false)
+				errs := i.Validate()
+				So(len(errs), ShouldEqual, 1)
+				So(errs[0].Error(), ShouldEqual, "ledger1: invalid actor id. cocoon or identity id cannot be an empty string")
+			})
+
+			Convey("Should return error if legder rule actor's privileges contains invalid privilege", func() {
+				i := NewInterpreter(map[string]interface{}{
+					"ledger1": map[string]string{
+						"@identity": "invalid",
+					},
+				}, false)
+				errs := i.Validate()
+				So(len(errs), ShouldEqual, 1)
+				So(errs[0].Error(), ShouldEqual, "ledger1: ledger actor contains an invalid privilege (invalid)")
+			})
+
+			Convey("Should validate successfully if ledger rule type is valid and value is valid privilege", func() {
+				i := NewInterpreter(map[string]interface{}{"ledger1": "allow deny"}, false)
+				errs := i.Validate()
+				So(len(errs), ShouldEqual, 0)
+			})
+
+		})
+
+		Convey(".IsAllowed. Should successfully return expected results", func() {
+
+			// rules, ledger name, actor id, operation, default policy, expected result
+			var cases = [][]interface{}{
+				[]interface{}{
+					map[string]interface{}{}, "ledger1", "actor_id", types.TxPut, true, true,
+				},
+				[]interface{}{
+					map[string]interface{}{}, "ledger1", "actor_id", types.TxPut, false, false,
+				},
+				[]interface{}{
+					map[string]interface{}{
+						"*": "allow deny",
+					}, "ledger1", "actor_id", types.TxPut, false, false,
+				},
+				[]interface{}{
+					map[string]interface{}{
+						"*": "allow deny",
+					}, "ledger1", "actor_id", types.TxPut, true, false,
+				},
+				[]interface{}{
+					map[string]interface{}{
+						"*": "deny",
+					}, "ledger1", "actor_id", types.TxPut, true, false,
+				},
+				[]interface{}{
+					map[string]interface{}{
+						"*": "deny",
+					}, "ledger1", "actor_id", types.TxGet, true, false,
+				},
+				[]interface{}{
+					map[string]interface{}{
+						"*": "allow",
+					}, "ledger1", "actor_id", types.TxGet, true, true,
+				},
+				[]interface{}{
+					map[string]interface{}{
+						"*": "allow-put",
+					}, "ledger1", "actor_id", types.TxGet, false, false,
+				},
+				[]interface{}{
+					map[string]interface{}{
+						"*": "allow-put",
+					}, "ledger1", "actor_id", types.TxPut, false, true,
+				},
+				[]interface{}{
+					map[string]interface{}{
+						"*": "allow-put",
+					}, "ledger1", "actor_id", types.TxGet, true, true,
+				},
+				[]interface{}{
+					map[string]interface{}{
+						"*": "allow-put",
+					}, "ledger1", "actor_id", types.TxGet, false, false,
+				},
+				[]interface{}{
+					map[string]interface{}{
+						"*": "allow-put",
+					}, "ledger1", "actor_id", types.TxPut, false, true,
+				},
+				[]interface{}{
+					map[string]interface{}{
+						"*": "allow-put deny-put",
+					}, "ledger1", "actor_id", types.TxPut, false, false,
+				},
+				[]interface{}{
+					map[string]interface{}{
+						"*": "allow-put deny-put allow",
+					}, "ledger1", "actor_id", types.TxPut, false, true,
+				},
+				[]interface{}{
+					map[string]interface{}{
+						"*":       "allow",
+						"ledger1": "deny",
+					}, "ledger1", "actor_id", types.TxPut, true, false,
+				},
+				[]interface{}{
+					map[string]interface{}{
+						"*":       "deny",
+						"ledger1": "allow",
+					}, "ledger1", "actor_id", types.TxPut, false, true,
+				},
+				[]interface{}{
+					map[string]interface{}{
+						"*":       "deny",
+						"ledger2": "allow",
+					}, "ledger1", "actor_id", types.TxPut, true, false,
+				},
+				[]interface{}{
+					map[string]interface{}{
+						"*": "allow",
+						"ledger1": map[string]string{
+							"actor1": "deny",
+						},
+					}, "ledger1", "actor1", types.TxPut, true, false,
+				},
+				[]interface{}{
+					map[string]interface{}{
+						"ledger1": map[string]string{
+							"actor1": "deny",
+						},
+					}, "ledger1", "actor1", types.TxPut, true, false,
+				},
+				[]interface{}{
+					map[string]interface{}{
+						"ledger1": map[string]string{
+							"actor1": "allow",
+						},
+					}, "ledger1", "actor1", types.TxPut, true, true,
+				},
+			}
+
+			for _, c := range cases {
+				i := NewInterpreter(c[0].(map[string]interface{}), c[4].(bool))
+				expected := i.IsAllowed(c[1].(string), c[2].(string), c[3].(string))
+				So(expected, ShouldEqual, c[5].(bool))
+			}
+		})
+	})
+}
