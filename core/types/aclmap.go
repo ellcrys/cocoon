a9def953859442bf2326eed25de8941d5ecce42a
Kennedy Idialu <kennedyidialu@gmail.com>
2017-04-10 08:11:55 +0100

    Added `Remove` method to remove rules.
    Fixed casting issues.

--- core/types/aclmap.go
+++ core/types/aclmap.go
@@ -24,17 +24,20 @@ func (m ACLMap) Add(target, privileges string) error {
 	targetParts := strings.Split(target, ".")
 	if len(targetParts) == 1 {
 		m[targetParts[0]] = privileges
-	} else if len(targetParts) == 2 {
-		if m[targetParts[0]] != nil {
-			if _, ok := m[targetParts[0]].(string); ok {
-				m[targetParts[0]] = map[string]string{
+	} else if len(targetParts) == 2 { // [ledgerName, actorID]
+		if m[targetParts[0]] != nil { // ledger name already exists and has value
+			if _, ok := m[targetParts[0]].(string); ok { // has string value
+				m[targetParts[0]] = map[string]string{ // replace with new map structure
 					targetParts[1]: privileges,
 				}
 			}
-			if innerMap, ok := m[targetParts[0]].(map[string]interface{}); ok {
-				innerMap[targetParts[1]] = privileges
+			if innerMap, ok := m[targetParts[0]].(map[string]string); ok { // hash map value
+				innerMap[targetParts[1]] = privileges // update existing map value
 			}
-		} else {
+			if innerMap, ok := m[targetParts[0]].(map[string]interface{}); ok { // hash map value
+				innerMap[targetParts[1]] = privileges // update existing map value
+			}
+		} else { // does not exist, free to add map value
 			m[targetParts[0]] = map[string]string{
 				targetParts[1]: privileges,
 			}
@@ -43,4 +46,30 @@ func (m ACLMap) Add(target, privileges string) error {
 		return fmt.Errorf("target format is invalid")
 	}
 	return nil
+}
+
+// Remove removes a target and its value
+func (m ACLMap) Remove(target string) {
+	if m == nil {
+		return
+	}
+	targetParts := strings.Split(target, ".")
+	if len(targetParts) == 1 {
+		delete(m, targetParts[0])
+	} else {
+		if m[targetParts[0]] != nil {
+			if innerMap, ok := m[targetParts[0]].(map[string]interface{}); ok {
+				delete(innerMap, targetParts[1])
+				if len(innerMap) == 0 { // if target is now empty, just remove it entirely
+					delete(m, targetParts[0])
+				}
+			}
+			if innerMap, ok := m[targetParts[0]].(map[string]string); ok {
+				delete(innerMap, targetParts[1])
+				if len(innerMap) == 0 { // if target is now empty, just remove it entirely
+					delete(m, targetParts[0])
+				}
+			}
+		}
+	}
 }
--- core/types/aclmap_test.go
+++ core/types/aclmap_test.go
@@ -69,5 +69,82 @@ func TestACLMap(t *testing.T) {
 				}))
 			})
 		})
+
+		Convey(".Remove", func() {
+
+			Convey("Should successfully remove an existing, non-actor specific rule", func() {
+				aclMap := NewACLMap(map[string]interface{}{
+					"*": "allow",
+					"ledger1": map[string]string{
+						"@identity_id": "allow",
+						"cocoon_id":    "allow",
+					},
+				})
+				aclMap.Remove("*")
+				So(aclMap, ShouldResemble, ACLMap(map[string]interface{}{
+					"ledger1": map[string]string{
+						"@identity_id": "allow",
+						"cocoon_id":    "allow",
+					},
+				}))
+				So(aclMap, ShouldNotResemble, ACLMap(map[string]interface{}{
+					"*": "allow",
+					"ledger1": map[string]string{
+						"@identity_id": "allow",
+						"cocoon_id":    "allow",
+					},
+				}))
+			})
+
+			Convey("Should successfully remove an existing, actor specific rule and leaving actor rules intact", func() {
+				aclMap := NewACLMap(map[string]interface{}{
+					"*": "allow",
+					"ledger1": map[string]string{
+						"@identity_id": "allow",
+						"cocoon_id":    "allow",
+					},
+				})
+				aclMap.Remove("ledger1.cocoon_id")
+				So(aclMap, ShouldResemble, ACLMap(map[string]interface{}{
+					"*": "allow",
+					"ledger1": map[string]string{
+						"@identity_id": "allow",
+					},
+				}))
+				So(aclMap, ShouldNotResemble, ACLMap(map[string]interface{}{
+					"*": "allow",
+					"ledger1": map[string]string{
+						"@identity_id": "allow",
+						"cocoon_id":    "allow",
+					},
+				}))
+			})
+
+			Convey("Should successfully remove all actor-specific rule if target is removed", func() {
+				aclMap := NewACLMap(map[string]interface{}{
+					"*": "allow",
+					"ledger1": map[string]string{
+						"@identity_id": "allow",
+						"cocoon_id":    "allow",
+					},
+				})
+				aclMap.Remove("ledger1")
+				So(aclMap, ShouldResemble, ACLMap(map[string]interface{}{
+					"*": "allow",
+				}))
+				aclMap = NewACLMap(map[string]interface{}{
+					"*": "allow",
+					"ledger1": map[string]string{
+						"@identity_id": "allow",
+						"cocoon_id":    "allow",
+					},
+				})
+				aclMap.Remove("ledger1.@identity_id")
+				aclMap.Remove("ledger1.cocoon_id")
+				So(aclMap, ShouldResemble, ACLMap(map[string]interface{}{
+					"*": "allow",
+				}))
+			})
+		})
 	})
 }
