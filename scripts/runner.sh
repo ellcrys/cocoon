5f124da14c718a76ea12f2591f85544336c70d48
Kennedy Idialu <kennedyidialu@gmail.com>
2017-04-18 07:37:52 +0100

    use exec

--- scripts/runner.sh
+++ scripts/runner.sh
@@ -4,24 +4,24 @@
 set -e
 echo "Runner.sh has started"
 
-term_handler() {
-    if [ $pid -ne 0 ]; then
-        kill -SIGTERM "$pid"
-        wait "$pid"
-    fi
-    exit 143; 
-}
+# term_handler() {
+#     if [ $pid -ne 0 ]; then
+#         kill -SIGTERM "$pid"
+#         wait "$pid"
+#     fi
+#     exit 143; 
+# }
 
-trap 'kill ${!}; term_handler' SIGTERM SIGINT
+# trap 'kill ${!}; term_handler' SIGTERM SIGINT
 
 # Fetch the script and run it.
 rm -f $RUN_SCRIPT_NAME
 wget $RUN_SCRIPT_URL
 chmod +x $RUN_SCRIPT_NAME
-./$RUN_SCRIPT_NAME &
-pid=$!
+exec ./$RUN_SCRIPT_NAME
+# pid=$!
 
-while true
-do
-  tail -f /dev/null & wait ${!}
-done
+# while true
+# do
+#   tail -f /dev/null & wait ${!}
+# done
