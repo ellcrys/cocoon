1ca7e644f8c2fd4e22d833ff7b7ad4a2a51186ce
Kennedy Idialu <kennedyidialu@gmail.com>
2017-04-19 17:46:06 +0100

    supports execution of pre-built binary in production environment

--- scripts/cfg/api/run.sh
+++ scripts/cfg/api/run.sh
@@ -1,16 +1,25 @@
 # Run an API. 
+set -e 
 
-repoParent="/go/src/github.com/ncodes"
-mkdir -p $repoParent
-cd $repoParent
+version="master"
 
-# pull cocoon source
-branch="master"
-git clone --depth=1 -b $branch https://github.com/ncodes/cocoon
-cd cocoon 
-git checkout $branch
+# Fetch and build API from source in dev/test environemtn
+if [ $ENV != "production" ]; then
+    repoParent="/go/src/github.com/ncodes"
+    mkdir -p $repoParent
+    cd $repoParent
+    git clone --depth=1 -b $version https://github.com/ncodes/cocoon
+    cd cocoon 
+    git checkout $version
+    govendor fetch -v +out
+    go build -o $GOPATH/bin/api core/api/main.go
+else
+     # Fetch pre-built binary 
+    printf "> Downloading pre-built binary [version: $VERSION]\n"
+    wget "https://storage.googleapis.com/krogan/api_${VERSION}.zip"
+    unzip "api_${VERSION}.zip"
+    mv api $GOPATH/bin/api
+fi
 
 # start the api
-govendor fetch -v +out
-go build -o api core/api/main.go
-./api start
+$GOPATH/bin/api start
