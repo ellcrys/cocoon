ac7e300c881d0fc0cc64a5c02a2044ffa5a9f22b
Kennedy Idialu <kennedyidialu@gmail.com>
2017-04-21 14:35:04 +0100

    Added more test cases

--- core/lock/consul_lock_test.go
+++ core/lock/consul_lock_test.go
@@ -3,15 +3,25 @@ package lock
 import (
 	"testing"
 
+	"github.com/ellcrys/util"
 	. "github.com/smartystreets/goconvey/convey"
 )
 
 func TestFunc(t *testing.T) {
 	Convey("ConsulLock", t, func() {
+
 		Convey(".AcquireLock", func() {
-			l := NewConsulLock()
-			err := l.Acquire("some_key")
-			So(err, ShouldBeNil)
+			Convey("Should successfully acquire a lock", func() {
+				key := util.RandString(10)
+				l := NewConsulLock()
+				err := l.Acquire(key)
+				So(err, ShouldBeNil)
+
+				Convey("Should have no problem calling re-acquiring a lock as long as TTL has not passed", func() {
+					err := l.Acquire(key)
+					So(err, ShouldBeNil)
+				})
+			})
 		})
 	})
 }
