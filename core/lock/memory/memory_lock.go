ddc4dc755135e0a5fe087c49cd4ba74d74f0ce85
Kennedy Idialu <kennedyidialu@gmail.com>
2017-04-21 18:29:49 +0100

    Added `MemoryLock` which is a memory based implementation of the Lock interface. Useful for testing.

--- core/lock/memory/memory_lock.go
+++ core/lock/memory/memory_lock.go
@@ -14,8 +14,14 @@ import (
 var LockTTL = time.Duration(10 * time.Second)
 
 // Global store for locked keys
-var lockedKeys map[string]int64
+var lockedKeys = make(map[string]*LockValue)
 var lockKeyMu = sync.Mutex{}
+
+// LockValue represents a lock value
+type LockValue struct {
+	Session string
+	Exp     time.Time
+}
 
 // Lock provides lock functionalities backed by memory.
 type Lock struct {
@@ -36,41 +42,82 @@ func NewLock(key string) *Lock {
 // Acquire acquires a lock. A time-to-live time is set
 // on the lock to ensure the lock is invalidated after the time is passed.
 func (l *Lock) Acquire() error {
-	if _, ok := lockedKeys[l.state["key"].(string)]; !ok {
-		return types.ErrLockAlreadyAcquired
+
+	if l.state["lock_session"].(string) == "" {
+		l.state["lock_session"] = util.UUID4()
 	}
-	l.state["lock_session"] = util.UUID4()
-	lockKey := fmt.Sprintf("%s.%s", l.state["lock_session"].(string), l.state["key"].(string))
-	if _, ok := l.state[lockKey]; ok {
-		return types.ErrLockAlreadyAcquired
+
+	lockKey := l.state["key"].(string)
+
+	lockKeyMu.Lock()
+	defer lockKeyMu.Unlock()
+	if lockValue, hasKey := lockedKeys[lockKey]; hasKey {
+		if lockValue.Session != l.state["lock_session"].(string) {
+			return types.ErrLockAlreadyAcquired
+		}
 	}
-	lockedKeys[lockKey] = time.Now().UTC().Add(LockTTL).Unix()
+
+	lockedKeys[lockKey] = &LockValue{
+		Session: l.state["lock_session"].(string),
+		Exp:     time.Now().UTC().Add(LockTTL),
+	}
+
 	return nil
 }
 
-// Release release a lock on the key
+// Release release a lock on the key. Returns nil of lock
+// on the current key does not exist
 func (l *Lock) Release() error {
 	lockKeyMu.Lock()
-	delete(lockedKeys, fmt.Sprintf("%s.%s", l.state["lock_session"].(string), l.state["key"].(string)))
-	lockKeyMu.Unlock()
+	defer lockKeyMu.Unlock()
+	key := l.state["key"].(string)
+	if lockVal, hasLock := lockedKeys[key]; hasLock {
+		if lockVal.Session == l.state["lock_session"].(string) {
+			delete(lockedKeys, key)
+		}
+	}
 	return nil
 }
 
 // IsAcquirer checks whether this lock instance is the acquirer of the lock on a specific key
 func (l *Lock) IsAcquirer() error {
+
 	if len(l.state["key"].(string)) == 0 {
 		return fmt.Errorf("key is not set")
 	}
 
-	lockKey := fmt.Sprintf("%s.%s", l.state["lock_session"].(string), l.state["key"].(string))
-	if _, ok := l.state[lockKey]; !ok {
-		return types.ErrLockNotAcquired
+	key := l.state["key"].(string)
+	if lockVal, hasLock := lockedKeys[key]; hasLock {
+		if lockVal.Session == l.state["lock_session"].(string) {
+			return nil
+		}
 	}
 
-	return nil
+	return types.ErrLockNotAcquired
 }
 
 // GetState returns the lock state
 func (l *Lock) GetState() map[string]interface{} {
 	return l.state
+}
+
+// StartLockWatcher starts a go routine that checks the locked key list
+// and removes expired locks. Returns a function to stop the lock watcher
+func StartLockWatcher() func() {
+	var stop = false
+	go func() {
+		for !stop {
+			lockKeyMu.Lock()
+			for k, lockVal := range lockedKeys {
+				if time.Now().UTC().After(lockVal.Exp) {
+					delete(lockedKeys, k)
+				}
+			}
+			lockKeyMu.Unlock()
+			time.Sleep(1 * time.Second)
+		}
+	}()
+	return func() {
+		stop = true
+	}
 }
--- core/lock/memory/memory_lock_test.go
+++ core/lock/memory/memory_lock_test.go
@@ -1,8 +1,12 @@
 package memory
 
 import (
+	"fmt"
 	"testing"
+	"time"
 
+	"github.com/ellcrys/util"
+	"github.com/ncodes/cocoon/core/types"
 	. "github.com/smartystreets/goconvey/convey"
 )
 
@@ -10,8 +14,70 @@ func TestMemoryLock(t *testing.T) {
 	Convey("MemoryLock", t, func() {
 		Convey(".AcquireLock", func() {
 			Convey("Should successfully acquire a lock", func() {
-				
+				key := util.RandString(10)
+				l := NewLock(key)
+				err := l.Acquire()
+				So(err, ShouldBeNil)
+
+				Convey("Should have no problem re-acquiring a lock as long as TTL has not passed", func() {
+					err := l.Acquire()
+					So(err, ShouldBeNil)
+				})
+
+				Convey("Should fail if lock has already been acquired by a different session", func() {
+					l := NewLock(key)
+					err := l.Acquire()
+					So(err, ShouldResemble, types.ErrLockAlreadyAcquired)
+				})
 			})
+		})
+
+		Convey(".ReleaseLock", func() {
+			Convey("Should successfully release an acquired lock", func() {
+				key := util.RandString(10)
+				l := NewLock(key)
+				err := l.Acquire()
+				So(err, ShouldBeNil)
+				err = l.Release()
+				So(err, ShouldBeNil)
+
+				Convey("Should successfully acquire a released lock", func() {
+					err := l.Acquire()
+					So(err, ShouldBeNil)
+				})
+			})
+		})
+
+		Convey(".IsAcquirer", func() {
+
+			Convey("Should return error if lock has no previously acquired key", func() {
+				l := NewLock("")
+				err := l.IsAcquirer()
+				So(err, ShouldResemble, fmt.Errorf("key is not set"))
+			})
+
+			Convey("Should return nil if lock is still the acquirer of a lock on it's key", func() {
+				key := util.RandString(10)
+				l := NewLock(key)
+				err := l.Acquire()
+				So(err, ShouldBeNil)
+				err = l.IsAcquirer()
+				So(err, ShouldBeNil)
+			})
+
+			Convey("Should return err if lock is no longer acquired due to TTL being reached", func() {
+				key := util.RandString(10)
+				l := NewLock(key)
+				LockTTL = 2 * time.Second
+				err := l.Acquire()
+				So(err, ShouldBeNil)
+				cancel := StartLockWatcher()
+				time.Sleep(4 * time.Second)
+				cancel()
+				err = l.IsAcquirer()
+				So(err, ShouldResemble, types.ErrLockNotAcquired)
+			})
+
 		})
 	})
 }
