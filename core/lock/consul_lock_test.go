a043154eb5a46c791b4a30a3e83146641eef2063
Kennedy Idialu <kennedyidialu@gmail.com>
2017-04-21 14:42:37 +0100

    Added more test

--- core/lock/consul_lock_test.go
+++ core/lock/consul_lock_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/ellcrys/util"
+	"github.com/ncodes/cocoon/core/types"
 	. "github.com/smartystreets/goconvey/convey"
 )
 
@@ -20,6 +21,12 @@ func TestFunc(t *testing.T) {
 				Convey("Should have no problem calling re-acquiring a lock as long as TTL has not passed", func() {
 					err := l.Acquire(key)
 					So(err, ShouldBeNil)
+				})
+
+				Convey("Should fail if lock has already been acquired by a different session", func() {
+					l := NewConsulLock()
+					err := l.Acquire(key)
+					So(err, ShouldResemble, types.ErrLockAlreadyAcquired)
 				})
 			})
 		})
