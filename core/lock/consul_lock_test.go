86dbf5303f7043ef8d780703660433205c5e585a
Kennedy Idialu <kennedyidialu@gmail.com>
2017-04-21 16:00:21 +0100

    increase time

--- core/lock/consul_lock_test.go
+++ core/lock/consul_lock_test.go
@@ -72,7 +72,7 @@ func TestFunc(t *testing.T) {
 				l := NewConsulLock()
 				err := l.Acquire(key)
 				So(err, ShouldBeNil)
-				time.Sleep(17 * time.Second)
+				time.Sleep(20 * time.Second)
 				err = l.IsAcquirer()
 				So(err, ShouldResemble, types.ErrLockNotAcquired)
 			})
